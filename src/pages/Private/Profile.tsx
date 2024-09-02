60ca78fa08c063b7a6436f6df384ef8dcd168f8e
jorge vergara <jorgevergarasou@gmail.com>
2024-09-02 15:00:30 -0300

    perfil y update usuario 02-09

--- src/pages/Private/Profile.tsx
+++ src/pages/Private/Profile.tsx
@@ -1,37 +1,76 @@
 import { useSelector } from "react-redux";
 import { AppStore } from "../../redux/store";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { profileService } from "../../services";
 
 function ProfilePage() {
   const user = useSelector((state: AppStore) => state.user);
   const apiUrl = `${import.meta.env.VITE_API_BASE_URL}`;
-  
+
   const [newName, setNewName] = useState('');
   const [newEmail, setNewEmail] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [currentPassword, setCurrentPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [profileData, setProfileData] = useState<any>(null); // Estado para los datos del perfil
+
+  // Función para obtener datos del perfil
+  const fetchProfileData = async () => {
+    if (!user.token) return;
+
+    try {
+      const response = await profileService(user.token);
+      setProfileData(response.data.profile); // Asigna los datos del perfil obtenidos al estado
+    } catch (error: any) {
+      console.error('Error al obtener el perfil:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProfileData();
+  }, []);
 
   const handleUpdate = async (field: string) => {
-    try {
-      const updateData: any = {}; 
+   
+    const fieldValidations: Record<string, { value: string; message: string }> = {
+      name: { value: newName, message: 'El campo nombre no puede estar vacío.' },
+      email: { value: newEmail, message: 'El campo email no puede estar vacío.' },
+      password: { value: newPassword, message: 'El campo contraseña no puede estar vacío.' },
+    };
   
-      if (field === 'name') updateData.name = newName;
-      if (field === 'email') updateData.email = newEmail;
-      if (field === 'password') updateData.password = newPassword;
-      if (field === 'currentPassword') updateData.currentPassword = currentPassword;
+    // Verifica si el campo que se está actualizando tiene un valor vacío
+    if (fieldValidations[field] && fieldValidations[field].value.trim() === '') {
+      setMessage(fieldValidations[field].message);
+      return;
+    }
+  
+    // Crea el objeto de datos a actualizar
+    const updateData: any = { currentPassword: currentPassword };
+  
+    // Asigna el valor al campo correspondiente
+    if (field in fieldValidations) {
+      updateData[field] = fieldValidations[field].value;
+    }
+  
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${user.token}`,
+    };
+    console.log(updateData);
+    
+      try {
+      const resUpdateUser = await axios.patch(`${apiUrl}/auth/updateUser/${user.idUser}`, updateData, { headers });
 
-      
-      const headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user.token}`,
-      };
 
-      const response = await axios.patch(`${apiUrl}/auth/updateUser/${user.idUser}`, updateData, { headers });
+      setMessage(resUpdateUser.data.message);
 
-      console.log('Respuesta del servidor:', response.data);
-      setMessage(response.data.message);
+      fetchProfileData();
+
+      setNewName('');
+      setNewEmail('');
+      setNewPassword('');
+      setCurrentPassword('');
 
     } catch (error) {
       console.error('Error en la actualización:', error);
@@ -39,71 +78,83 @@ function ProfilePage() {
     }
   };
 
+
   return (
     <div className="flex flex-col text-center h-full mx-auto px-2">
       <div><h1>Perfil {user.role}</h1></div>
       <div className="grid grid-cols-5 gap-2">
-        <div className="col-span-1"></div>
+        <div className="col-span-1">
+
+
+        </div>
         <div className="col-span-3 bg-gray-300 h-full w-full rounded-t-xl p-8">
+
+
+
           <ul className="space-y-4">
 
-            {/* Campo para cambiar el nombre */}
+
+            {profileData && (
+
+              <>
+
+                <li className="flex items-center">
+                  <div className="flex flex-col">
+                    <label>Nombre: {profileData.name}</label>
+                    <input
+                      type="text"
+                      placeholder="Cambiar nombre"
+                      className="bg-gray-200 rounded-lg p-2"
+                      value={newName}
+                      onChange={(e) => setNewName(e.target.value)} />
+                    <button className="bg-green-300 rounded-lg p-2 mt-2" onClick={() => handleUpdate('name')}>Cambiar nombre</button>
+                  </div>
+                </li>
+                <li className="flex items-center">
+                  <div className="flex flex-col">
+                    <label>Email: {profileData.email}</label>
+                    <input
+                      type="email"
+                      placeholder="Cambiar email"
+                      className="bg-gray-200 rounded-lg p-2"
+                      value={newEmail}
+                      onChange={(e) => setNewEmail(e.target.value)}
+                    />
+                    <button className="bg-green-300 rounded-lg p-2 mt-2" onClick={() => handleUpdate('email')}>Cambiar email</button>
+                  </div>
+                </li>
+                <li className="flex items-center">
+                  <div className="flex flex-col">
+                    <label>Cambiar contraseña:</label>
+                    <input
+                      type="password"
+                      placeholder="Nueva contraseña"
+                      className="bg-gray-200 rounded-lg p-2"
+                      value={newPassword}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                    <button className="bg-green-300 rounded-lg p-2 mt-2" onClick={() => handleUpdate('password')}>Cambiar contraseña</button>
+                  </div>
+                </li>
+
+              </>
+            )}
             <li className="flex items-center">
 
 
-            <div className="flex flex-col">
+              <div className="flex flex-col">
                 <div>
                   Contraseña actual:
-                  <input 
-                    type="password" 
-                    placeholder="Ingresa tu contraseña actual" 
-                    className="bg-gray-200 rounded-lg p-2"
+                  <input
+                    type="password"
+                    placeholder="Ingresa tu contraseña actual"
+                    className="bg-gray-200 rounded-lg p-2 "
+                    value={currentPassword}
                     onChange={(e) => setCurrentPassword(e.target.value)}
                   />
                 </div>
               </div>
-
-
-              <div className="flex flex-col">
-                <label>Nombre actual: {user.name}</label>
-                <input 
-                  type="text" 
-                  placeholder="Cambiar nombre" 
-                  className="bg-gray-200 rounded-lg p-2"
-                  onChange={(e) => setNewName(e.target.value)}
-                />
-                <button className="bg-green-300 rounded-lg p-2 mt-2" onClick={() => handleUpdate('name')}>Cambiar nombre</button>
-              </div>
             </li>
-
-            {/* Campo para cambiar el email */}
-            <li className="flex items-center">
-              <div className="flex flex-col">
-                <label>Email actual: {user.email}</label>
-                <input 
-                  type="email" 
-                  placeholder="Cambiar email" 
-                  className="bg-gray-200 rounded-lg p-2"
-                  onChange={(e) => setNewEmail(e.target.value)}
-                />
-                <button className="bg-green-300 rounded-lg p-2 mt-2" onClick={() => handleUpdate('email')}>Cambiar email</button>
-              </div>
-            </li>
-
-            {/* Campo para cambiar la contraseña */}
-            <li className="flex items-center">
-              <div className="flex flex-col">
-                <label>Cambiar contraseña:</label>
-                <input 
-                  type="password" 
-                  placeholder="Nueva contraseña" 
-                  className="bg-gray-200 rounded-lg p-2"
-                  onChange={(e) => setNewPassword(e.target.value)}
-                />
-                <button className="bg-green-300 rounded-lg p-2 mt-2" onClick={() => handleUpdate('password')}>Cambiar contraseña</button>
-              </div>
-            </li>
-
           </ul>
 
           {/* Mostrar mensaje de éxito o error */}
